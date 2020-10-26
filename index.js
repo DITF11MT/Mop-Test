5ad6d1fcbab5dac6947d0e9e6cba037f29af8021
talhaalshafeai44 <tal7a.tmam@gmail.com>
2020-10-26 16:44:21 +0300

    port

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+	port: '3306'
 });
 
 var connection;
