697bfbba8628741cbe08b79d123d13796e1c1abc
talhaalshafeai44 <tal7a.tmam@gmail.com>
2020-10-26 16:15:48 +0300

    adding handler

--- index.js
+++ index.js
@@ -5,11 +5,38 @@ const slash = require('express-slash');
 const Joi = require('joi');
 
 const con = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    user: "bddcf93f062fda",
-    password: "b8c562de",
-    database: "heroku_bd1fb1417f7c391"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(con); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+
 
 const app = express();
 app.enable('strict routing');
